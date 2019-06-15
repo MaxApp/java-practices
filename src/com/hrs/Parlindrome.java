8963b4a10fa542766a9922baeeb30c79dd466fd9
MaxApp <max2030tokyo@hotmail.com>
2019-06-15 10:52:38 +0800

    子集判断
    对排序后的数据，进行子集判断

--- src/com/hrs/Parlindrome.java
+++ src/com/hrs/Parlindrome.java
@@ -1,11 +1,14 @@
 package com.hrs;
 
 /*
- *  回文检测
+ *  1.回文检测
+ *  2.对排序后的数组，进行子集判断
  */
 public class Parlindrome {
 
 	public static void main(String[] args) {
+		
+		/*
 		String param = args[0];
 		int front = 0;
         int end = param.length()-1;
@@ -22,8 +25,38 @@ public class Parlindrome {
             System.out.println("yes");
         }else{
             System.out.println("no");
-        } 
+        } */
 
+		int[] a={2,3,3,4,6,8,9,13,15,23,24,28,32,36,54,69,72,75,81,88,89,94,96,97};
+		int[] b={5,6,13,24,54,72};
+		
+		if(isSubset(a,b)){
+			System.out.println("true");
+		}else{
+			System.out.println("false");
+		}
+		
+	}
+	
+	/*
+	 * 对排序后的两个数组
+	 * 判断 small 是否是 big 的子集
+	 */
+	static boolean isSubset(int[] big, int[] small){
+		int blen=big.length;
+		int slen=small.length;
+		int j=0;
+		for(int i=0;i<slen;i++,j++){
+			for(;j<blen;j++){
+				if(small[i]==big[j]){
+					break;
+				}
+			}
+			
+			if(j==blen)
+				return false;
+		}
+		return true;
 	}
 
 }
